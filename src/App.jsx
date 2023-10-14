5d7837514e31541144865e351a78d2e56555dfee
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-10-14 16:25:24 -0600

    Add api

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@
 import './App.css'
 import BigDay from './components/BigDay/bigDay'
 import BarraDerecha from './components/BarraDerecha/BarraDerecha'
+import { useCallback, useEffect } from 'react'
+import { API_CITY_URL } from './Constants/constants'
 
 
 
 function App() {
+  useEffect(() => {
+
+      fetch( API_CITY_URL ).then((res) => {
+        return res.json();
+      }).then((data) => console.log(data) )
+    
+      
+    //fetch( API_CITY_URL, 'get')
+  },[])
+
   return (
     <>
     <div className='contianer-padre'>
---
+++ src/Constants/constants.js
@@ -0,0 +1,6 @@
+export const key = "8a6e433d43da73161c186f1115f2cb4d"
+export const api_url = `api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=8a6e433d43da73161c186f1115f2cb4d`
+
+export const API_LATITUD =  `api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=8a6e433d43da73161c186f1115f2cb4d`
+
+export const API_CITY_URL = `api.openweathermap.org/data/2.5/forecast?q=Hidalgo,mx&appid=8a6e433d43da73161c186f1115f2cb4d`
